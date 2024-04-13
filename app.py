96dc85586fcfab7691491d85f1abcb25aa3c6ba8
sikehish <hisham0502@gmail.com>
2024-04-14 01:06:12 +0530

    Extracting data from every problem page

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, jsonify
+from bs4 import BeautifulSoup
 import requests
 
 app = Flask(__name__)
 
 @app.route('/leetcode')
 def get_leetcode_questions():
-    url = 'https://leetcode.com/api/problems/algorithms/'
+
+    ALGORITHMS_LINK = 'https://leetcode.com/api/problems/algorithms/'
+    PROBLEMS_URL = "https://leetcode.com/problems/"
+
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(ALGORITHMS_LINK, headers=headers)
     
     if response.status_code == 200:
         data = response.json() #OR json.loads(response.text)
@@ -20,6 +24,20 @@ def get_leetcode_questions():
             }
             for child in data["stat_status_pairs"] if not child["paid_only"]
         ]
+        # for link in links:
+        link=links[0]
+        response=requests.get(PROBLEMS_URL + link["title_slug"], headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        file = open("output.html", "w")
+        file.write(response.text)
+        file.close()
+
+        # box = soup.find('div', class_='elfjS')
+        # if box:
+        #     link["problem"] = box.get_text()
+        # else:
+        #     link["problem"] = "Problem description not found"
+
         return jsonify(links)
     else:
         return jsonify({'error': 'Failed to fetch LeetCode questions'}), response.status_code
