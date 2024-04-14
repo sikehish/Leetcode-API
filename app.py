ffd7f61c33f40e9eee2b6c732f0867af3bcbcfba
sikehish <hisham0502@gmail.com>
2024-04-14 23:03:49 +0530

    Used selenium driver to simulate broswer env and obtain the JS rendered data

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify
 from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
+import requests,os,json
+from time import sleep
+from selenium.webdriver.common.by import By
 
 app = Flask(__name__)
 
@@ -24,13 +27,14 @@ def get_leetcode_questions():
             }
             for child in data["stat_status_pairs"] if not child["paid_only"]
         ]
-        # for link in links:
-        link=links[0]
-        response=requests.get(PROBLEMS_URL + link["title_slug"], headers=headers)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        file = open("output.html", "w")
-        file.write(response.text)
-        file.close()
+        # # for link in links:
+        # link=links[0]
+        # url=PROBLEMS_URL + link["title_slug"]+"/description"
+        # response=requests.get(url, headers=headers)
+        # soup = BeautifulSoup(response.content)
+        # file = open("output.html", "w")
+        # file.write(response.text)
+        # file.close()
 
         # box = soup.find('div', class_='elfjS')
         # if box:
@@ -41,6 +45,21 @@ def get_leetcode_questions():
         return jsonify(links)
     else:
         return jsonify({'error': 'Failed to fetch LeetCode questions'}), response.status_code
+    
+@app.route('/html')
+def get_leetcode_html():
+    driver=webdriver.Chrome()
+    driver.get('https://leetcode.com/problems/two-sum/')
+    # Wait for the page to load completely (you might need to adjust the wait time)
+    driver.implicitly_wait(30)
+
+    html_content=driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+    # sleep(5)
+
+    problem_description = driver.find_element(By.CLASS_NAME, 'elfjS')
+    print(problem_description.text)
+    return jsonify({"content":problem_description.text})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
